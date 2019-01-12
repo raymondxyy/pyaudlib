585d1e4596b844e9d618d07fbf9f67ef8d97fde7
shangwuyao <shangwuy@andrew.cmu.edu>
2019-01-12 16:05:24 +0800

    Added doc for LSTM layers

--- audlib/nn/nn.py
+++ audlib/nn/nn.py
@@ -66,7 +66,7 @@ class DetMLP(Module):
         self.bias = bias
         self.relu = nn.ReLU()
         self.linears = nn.ModuleList(
-            [DetLinear(indim, bias=bias) for ii in range(1+self.nhidden)]
+            [DetLinear(indim, bias=bias) for ii in range(1 + self.nhidden)]
         )
 
     def forward(self, x):
@@ -119,7 +119,7 @@ class MLP(Module):
         outdims = hiddims + [outdim]
         self.linears = nn.ModuleList(
             [nn.Linear(indims[ii], outdims[ii], bias=bias)
-             for ii in range(1+self.nhidden)]
+             for ii in range(1 + self.nhidden)]
         )
 
     def forward(self, x):
@@ -139,9 +139,36 @@ class MLP(Module):
 
 
 class AdvancedLSTMCell(nn.LSTMCell):
-    # TODO: Add docstring
-    # Extend LSTMCell to learn initial state
+    """Extend LSTMCell to learn initial states."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a LSTMCell.
+
+        Parameters
+        ----------
+        input_size: The number of expected features in the input `x`
+        hidden_size: The number of features in the hidden state `h`
+        bias: If `False`, then the layer does not use bias weights `b_ih` and
+            `b_hh`. Default: ``True``
+
+        Inputs
+        ------
+        input, (h_0, c_0)
+        input of shape (batch, input_size): tensor containing input features
+        h_0 of shape (batch, hidden_size): tensor containing the initial hidden
+            state for each element in the batch.
+        c_0 of shape (batch, hidden_size): tensor containing the initial cell
+            state for each element in the batch.
+        If (h_0, c_0) is not provided, both h_0 and c_0 default to zero.
+
+        Outputs
+        -------
+        h_1 of shape (batch, hidden_size): tensor containing the next hidden
+            state for each element in the batch.
+        c_1 of shape (batch, hidden_size): tensor containing the next cell
+            state for each element in the batch.
+
+        """
         super(AdvancedLSTMCell, self).__init__(*args, **kwargs)
         self.h0 = nn.Parameter(torch.FloatTensor(1, self.hidden_size).zero_())
         self.c0 = nn.Parameter(torch.FloatTensor(1, self.hidden_size).zero_())
@@ -154,8 +181,61 @@ class AdvancedLSTMCell(nn.LSTMCell):
 
 
 class AdvancedLSTM(nn.LSTM):
-    # Class for learning initial hidden states when using LSTMs
+    """Extend LSTM to learn initial states."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a LSTM.
+
+        Parameters
+        ----------
+        input_size – The number of expected features in the input x
+        hidden_size – The number of features in the hidden state h
+        num_layers – Number of recurrent layers. E.g., setting num_layers=2
+            would mean stacking two LSTMs together to form a stacked LSTM,
+            with the second LSTM taking in outputs of the first LSTM and
+            computing the final results. Default: 1
+        bias – If False, then the layer does not use bias weights b_ih and
+            b_hh. Default: True
+        batch_first – If True, then the input and output tensors are provided
+            as (batch, seq, feature). Default: False
+        dropout – If non-zero, introduces a Dropout layer on the outputs of
+            each LSTM layer except the last layer, with dropout probability
+            equal to dropout. Default: 0
+        bidirectional – If True, becomes a bidirectional LSTM. Default: False
+
+        Inputs
+        ------
+        input, (h_0, c_0)
+        input of shape (seq_len, batch, input_size): tensor containing the
+            features of the input sequence. The input can also be a packed
+            variable length sequence.
+        h_0 of shape (num_layers * num_directions, batch, hidden_size):
+            tensor containing the initial hidden state for each element in the
+            batch. If the RNN is bidirectional, num_directions should be 2,
+            else it should be 1.
+        c_0 of shape (num_layers * num_directions, batch, hidden_size): tensor
+            containing the initial cell state for each element in the batch.
+        If (h_0, c_0) is not provided, both h_0 and c_0 default to zero.
+
+        Outputs
+        -------
+        output, (h_n, c_n)
+        output of shape (seq_len, batch, num_directions * hidden_size): tensor
+            containing the output features (h_t) from the last layer of the
+            LSTM, for each t. If a torch.nn.utils.rnn.PackedSequence has been
+            given as the input, the output will also be a packed sequence.
+        For the unpacked case, the directions can be separated using output.
+            view(seq_len, batch, num_directions, hidden_size), with forward
+            and backward being direction 0 and 1 respectively. Similarly,
+            the directions can be separated in the packed case.
+        h_n of shape (num_layers * num_directions, batch, hidden_size):
+            tensor containing the hidden state for t = seq_len.
+        Like output, the layers can be separated using h_n.view(num_layers,
+            num_directions, batch, hidden_size) and similarly for c_n.
+        c_n (num_layers * num_directions, batch, hidden_size): tensor
+            containing the cell state for t = seq_len
+
+        """
         super(AdvancedLSTM, self).__init__(*args, **kwargs)
         bi = 2 if self.bidirectional else 1
         self.h0 = nn.Parameter(torch.FloatTensor(
@@ -177,18 +257,21 @@ class AdvancedLSTM(nn.LSTM):
 
 
 class SequenceShuffle(nn.Module):
-    # Performs pooling for pBLSTM
+    """
+    Performs efficient pooling (using indexing) for pyramid LSTM without
+    iteration.
+    """
 
     @staticmethod
     def concate_sequence(sequence, length):
-        '''pyramid BiLSTM, merge consecutive time step together'''
-        shape = sequence.size()  # (S, B, D)
+        '''pyramid LSTM, merge consecutive time step together'''
+        shape = sequence.size()
 
         # efficient using indexing, don't need iteration
-        input_range = sequence.data.new(shape[0]//2).zero_().long()
+        input_range = sequence.data.new(shape[0] // 2).zero_().long()
         torch.arange(1, int(shape[0]), 2, out=input_range)
         input_concate = torch.cat(
-            (sequence[input_range-1], sequence[input_range]), 2)
+            (sequence[input_range - 1], sequence[input_range]), 2)
 
         length = np.array(length) // 2
 
@@ -197,18 +280,16 @@ class SequenceShuffle(nn.Module):
     def forward(self, seq):
         assert isinstance(seq, PackedSequence)
         h, seq_lengths = pad_packed_sequence(seq)
-
-        # pyramid BiLSTM, merge consecutive time step together
-        # input size should be (S, B, D)
         h, seq_lengths = self.concate_sequence(h, seq_lengths)
         h = pack_padded_sequence(h, seq_lengths)
         return h
 
 
 class PyramidalLSTM(AdvancedLSTM):
-    # Pyramidal LSTM
+    """Pyramidal LSTM could reduce the sequence length by half."""
 
     def __init__(self, *args, **kwargs):
+        """See AdvancedLSTM."""
         super(PyramidalLSTM, self).__init__(*args, **kwargs)
         self.shuffle = SequenceShuffle()
 
@@ -217,22 +298,39 @@ class PyramidalLSTM(AdvancedLSTM):
 
 
 class WeightdropLSTM(nn.LSTM):
-    # TODO: add docstring.
+    """
+    LSTM with dropout, apply dropout on the hidden-to-hidden weight matrix
+    and the input.
+    """
 
     @staticmethod
-    def generate_mask(p, inp=None):
+    def generate_mask(p, inp):
         """Generate mask for random drop of network weights.
 
         p is the drop rate, aka the probability of being 0
         modified from https://github.com/salesforce/awd-lstm-lm/blob/master/
         using .new() to initialize from the same device, much more efficient
         """
-        # TODO: how is inp=None valid?
-        # TODO: Remove variable.
-        return (Variable(inp.data.new(1, inp.size(1)).bernoulli_(1. - p)) / (1. - p)).expand_as(inp)
+        return (inp.data.new(1, inp.size(1)).bernoulli_(1. - p) / (1. - p))\
+            .expand_as(inp)
 
-    def __init__(self, input_size, hidden_size, dropout_weight, dropout_bet):
-        # TODO: Add docstring.
+    def __init__(self, input_size, hidden_size, dropout_weight, dropout_inp):
+        """Initialize a WeightdropLSTM.
+
+        Parameters
+        ----------
+        input_size: int
+            The number of expected features in the input x
+        hidden_size: int
+            The number of features in the hidden state h
+        dropout_weight: float
+            Introduces a Dropout layer on the hidden-to-hidden weight matrix
+            of LSTM, with dropout probability equal to dropout.
+        dropout_inp: float
+            Introduces a Dropout layer on the input matrix of LSTM, with
+            dropout probability equal to dropout.
+
+        """
         super(WeightdropLSTM, self).__init__(input_size=input_size,
                                              hidden_size=hidden_size,
                                              bidirectional=True)
@@ -240,21 +338,22 @@ class WeightdropLSTM(nn.LSTM):
         self.weight_hh_l0 = None
         del self._parameters['weight_hh_l0']
         self.dropout_layer = nn.Dropout(dropout_weight)
-        self.dropout_bet = dropout_bet
+        self.dropout_inp = dropout_inp
 
     def flatten_parameters(self):
-        # TODO: Add docstring.
-        # overwrite, prevent pytorch from putting all weight into a large chunk
+        """
+        Overwrite this method, which prevents PyTorch from putting all weight
+        into a large chunk.
+        """
         self._data_ptrs = []
 
     def forward(self, inp, hx=None):
         self.weight_hh_l0 = self.dropout_layer(self.old_weight_hh_l0)
-        raw_output = None
 
-        if self.training and self.dropout_bet != 0:
+        if self.training and self.dropout_inp != 0:
             input, batch_size = inp
             between_layer_mask = self.generate_mask(
-                p=self.dropout_bet, inp=input)
+                p=self.dropout_inp, inp=input)
             dropedinput = input * between_layer_mask
             inp = PackedSequence(dropedinput, batch_size)
 
@@ -268,12 +367,14 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.layers = nn.ModuleList()
         self.layers.append(nn.Conv1d(
-            in_channels=input_dim, out_channels=output_dim, padding=1, kernel_size=3, stride=1))
+            in_channels=input_dim, out_channels=output_dim, padding=1,
+            kernel_size=3, stride=1))
         self.layers.append(nn.ReLU())
         self.layers.append(nn.BatchNorm1d(output_dim))
         self.layers.append(nn.Dropout(0.1))
         self.layers.append(nn.Conv1d(
-            in_channels=output_dim, out_channels=output_dim, padding=1, kernel_size=3, stride=1))
+            in_channels=output_dim, out_channels=output_dim, padding=1,
+            kernel_size=3, stride=1))
         self.layers.append(nn.ReLU())
         self.layers.append(nn.BatchNorm1d(output_dim))
         self.layers.append(nn.Dropout(0.1))
