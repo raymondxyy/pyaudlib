61c7549d1c64109086bd96678dbcd083ed0ddcb7
Raymond Xia <raymondxia@cmu.edu>
2019-05-07 02:41:21 -0400

    Add STRFNet in WASPAA2019 paper. Modify STRFLayerFC to have more informative names. NOTE: this change breaks compatibility with models learned with the strfnet repo.

--- audlib/nn/strfnet.py
+++ audlib/nn/strfnet.py
@@ -1,10 +1,12 @@
-"""Convolutional Neural Network Layers."""
+"""STRFNet."""
+
 import math
 
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
+import torch.nn.functional as F
 
+from .nn import MLP
 from .signal import hilbert
 from ..sig.util import nextpow2
 
@@ -19,7 +21,7 @@ class STRFConv(nn.Module):
         Parameters
         ----------
         fr: int
-            Frame rate of the spectrogram in Hz.
+            Frame rate of the incoming spectrogram in Hz.
             e.g. spectrogram with 10ms hop size has frame rate 100Hz.
         bins_per_octave: int
             Number of frequency dimensions per octave in the spectrogram.
@@ -44,15 +46,11 @@ class STRFConv(nn.Module):
             Init. for learnable phase shift of time evolution in radians.
             Dimension must match `nkern` if specified.
 
-        See Also
-        --------
-        sig.spectemp.strf
-
         """
         super(STRFConv, self).__init__()
 
         # For printing
-        self.__rep = f"""STRF(fr={fr}, bins_per_octave={bins_per_octave},
+        self.__rep = f"""STRFConv(fr={fr}, bins_per_octave={bins_per_octave},
             suptime={suptime}, supoct={supoct}, nkern={nkern},
             rates={rates}, scales={scales}, phis={phis},
             thetas={thetas})"""
@@ -94,8 +92,8 @@ class STRFConv(nn.Module):
         rt = rate * t
         return rate * rt**2 * torch.exp(-3.5*rt) * torch.sin(2*math.pi*rt)
 
-    def strfs(self):
-        """Make STRFs using current parameters."""
+    def strfgen(self):
+        """Generate downward and upward STRFs using current parameters."""
         if self.supt.device != self.rates_.device:  # for first run
             self.supt = self.supt.to(self.rates_.device)
             self.supf = self.supf.to(self.rates_.device)
@@ -138,40 +136,49 @@ class STRFConv(nn.Module):
         """
         if len(sigspec.shape) == 2:  # expand batch dimension if single eg
             sigspec = sigspec.unsqueeze(0)
-        strfs = self.strfs().unsqueeze(1).type_as(sigspec)
+        strfs = self.strfgen().unsqueeze(1).type_as(sigspec)
         return F.conv2d(sigspec.unsqueeze(1), strfs, padding=self.padding)
 
     def __repr__(self):
         return self.__rep
 
 
-class STRFLayer(nn.Module):
-    """A fully convolutional layer with STRF kernels at the bottom."""
-    # NOTE: This version is UNSTABLE. Use with caution.
+class STRFLayerFC(nn.Module):
+    """A Fully Convolutional layer with STRF kernels at the bottom."""
     def __init__(self, fr, bins_per_octave, suptime, supoct, nkern,
-                 indim, outdim, kernel_size, nchan):
+                 indim, outdim, kernel_size, nchan_conv, nchan_out):
         """Construct a STRF layer from STRFConv and nonlinearities.
 
         The layer structure follows this flow:
-        0. N x T x F batch comes in.
-        1. N x K x T x F batch comes out of STRFConv.
+        0. N x T x F batch comes in, where:
+            N = batch size
+            T, F = indim
+        1. N x K x T x F batch comes out of STRFConv, where:
+            K = 2 x nkern (upward and downward drifting pairs)
+            The STRF kernel size is determined by fr, bpo, suptime, and supoct.
         2. N x K x T x F batch comes out of BatchNorm.
         3. N x K x T x F batch comes out of nonlinearity.
-        4. N x K x T'x F' batch comes out of Conv2d, where:
+        4. N x K' x T'x F' batch comes out of Conv2d, where:
             T' = largest integer below T that is an integer power of 2
             F' = largest integer below F that is an integer power of 2
-            If T != F, pool on the higher dimension only until they equal.
-        5. N x K x T' x F' batch comes out of Conv2d.
-        6. N x K x T''x F'' batch comes out of pooling, where:
-            T'' = T'//2; F'' = F'//2
-        7. Repeat step 5 and 6 until T'' and F'' reach 8.
+            K' = nchan_conv
+            The Conv2d kernel size is determined by kernel_size.
+        5. N x K' x T' x F' batch comes out of LayerNorm and ReLU.
+        6. Repeat step 4 and 5 until T', F' == outdim. Should that happen, also
+           make the output channel == nchan_out. This output will be the input
+           to MLP for classification.
+
+        CAVEATS:
+        `outdim` must be integer power of 2. This is for the convenience of
+        taking a stride of 2 to halve the feature dimension in each conv layer.
+
         """
-        super(STRFLayer, self).__init__()
+        super(STRFLayerFC, self).__init__()
         # Calculate input & output sizes in all layers
         itdim, ifdim = indim
         otdim, ofdim = outdim
         assert (nextpow2(otdim) == otdim) and (nextpow2(ofdim) == ofdim),\
-            "Output dimensions must be integer multiples of 2"
+            "Output dimensions must be integer power of 2"
         iosizes = [(itdim, ifdim)]  # input/output sizes in all layers
         while (itdim != otdim) or (ifdim != ofdim):
             if itdim != otdim:
@@ -179,32 +186,44 @@ class STRFLayer(nn.Module):
             if ifdim != ofdim:
                 ifdim = nextpow2(ifdim)//2
             iosizes.append((itdim, ifdim))
-        nlayers = len(iosizes) - 1  # exclude STRF layer
-        # linearly space between 2*nkern and nchan
-        nchans = [max((2*nkern)/(2**n), nchan) for n in range(len(iosizes))]
-        assert nchans[-1] >= nchan
+        nlayers = len(iosizes) - 1  # exclude STRFConv
+        nchans = [2*nkern] + [nchan_conv]*(nlayers-1) + [nchan_out]
 
-        # Construct all layers
-        layers = [STRFConv(fr, bins_per_octave, suptime, supoct, nkern)]
-        for ll in range(nlayers):
-            # fully convolutional layers here
+        # Construct all layers; start with STRF + pooling
+        iit, iif = iosizes[0]
+        oot, oof = iosizes[1]
+        if type(kernel_size) == int:
+            kernel_size = kernel_size, kernel_size
+
+        # Start with the STRFConv layer
+        self.strfconv = STRFConv(
+            fr, bins_per_octave, suptime, supoct, nkern,
+            rates=[(n % 3 + 1) for n in range(nkern)],
+            scales=[.3*(n % 5 + 1) for n in range(nkern)])
+
+        # Then fully convolutional layers here
+        fullyconv = []
+        iiconv = 0
+        for ll in range(iiconv, nlayers):
             iit, iif = iosizes[ll]
             oot, oof = iosizes[ll+1]
+            kkt, kkf = kernel_size  # changing kernel size doesn't help much
             sst = 1 if iit == oot else 2
             ssf = 1 if iif == oof else 2
-            fconv = [nn.Conv2d(nchans[ll], nchans[ll+1], kernel_size,
+            fconv = [nn.Conv2d(nchans[ll], nchans[ll+1], (kkt, kkf),
                      stride=(sst, ssf),
-                     padding=(self.padsize(iit, oot, kernel_size, sst),
-                     self.padsize(iif, oof, kernel_size, ssf))),
-                     #nn.BatchNorm2d(nchans[ll+1]),
+                     padding=(self.padsize(iit, oot, kkt, sst),
+                     self.padsize(iif, oof, kkf, ssf))),
+                     nn.LayerNorm([nchans[ll+1], oot, oof]),
                      nn.ReLU()
                      ]
-            layers.extend(fconv)
+            fullyconv.extend(fconv)
 
-        self.layers = nn.ModuleList(layers)
+        self.fullyconv = nn.ModuleList(fullyconv)
 
     def forward(self, x):
-        for layer in self.layers:
+        x = self.strfconv(x)
+        for layer in self.fullyconv:
             x = layer(x)
         return x
 
@@ -218,5 +237,80 @@ class STRFLayer(nn.Module):
         =>  padsize = ceil((stride(osize-1)-isize+ksize) / 2)
         """
         psize = math.ceil((stride*(osize-1) - isize + ksize) / 2)
-        assert osize == math.floor((isize + 2*psize - ksize)/stride + 1)
+        oosize = math.floor((isize + 2*psize - ksize)/stride + 1)
+        assert oosize == osize,\
+            f"{isize}-{osize}-{ksize}-{stride}-{psize}-{oosize}"
         return psize
+
+    @staticmethod
+    def outsize(isize, psize, ksize, stride):
+        """Calculate output size given input and other configs."""
+        return math.floor((isize + 2*psize - ksize)/stride + 1)
+
+
+class STRFNetWASPAA2019(nn.Module):
+    """A CNN+MLP classifier that has STRFLayers in the CNN."""
+
+    @staticmethod
+    def is_strf_param(nm):
+        """Check if a parameter name string is one of STRF parameters."""
+        return any(n in nm for n in ("rates_", "scales_", "phis_", "thetas_"))
+
+    def __init__(self, fr, bins_per_octave, suptimes, supocts, nkerns,
+                 num_classes, indim, mlp_hidden, outdim=(4, 4), kernel_size=5,
+                 nchan_conv=60, nchan_out=32):
+        """Instantiate a CNN with STRF extractors in the bottom layer.
+
+        Parameters
+        ----------
+        fr: int
+            Frame rate in Hz.
+        bins_per_octave: int
+            Number of bins per octave.
+        tdim: int
+            Time dimension of spectrogram feature.
+        fdim: int
+            Frequency dimension of spectrogram feature.
+        suptimes: list of float
+            Time support of STRFs.
+        supocts: list of float
+            Frequency support of STRFs.
+        nkerns: list of int
+            Number of STRFs per setup.
+        num_classes: int
+            N-way classification.
+        indim: tuple(int, int)
+            Feature input dimension.
+        mlp_hidden: list(int)
+            MLP hidden layer dimensions in a list.
+        outdim: tuple(int, int), (4, 4)
+            Output feature dimension coming out of convolutional layers.
+        kernel_size: int or tuple(int, int), (5, 5)
+            Kernel size of fully convolutional layer.
+        nchan_conv: int, 60
+            Number of output channels in each fully convolutional layer.
+        nchan_out: int, 32
+            Number of output channels in the final convolutional layer.
+
+        See Also
+        --------
+        STRFConv, STRFLayer
+
+        """
+        super(STRFNetWASPAA2019, self).__init__()
+        assert len(suptimes) == len(supocts) == len(nkerns)
+        self.strflayers = nn.ModuleList(
+            [STRFLayerFC(fr, bins_per_octave, t, f, k, indim, outdim,
+                         kernel_size, nchan_conv, nchan_out)
+                for t, f, k in zip(suptimes, supocts, nkerns)])
+        # Input to MLP will be flattened CNN output
+        inmlp = outdim[0] * outdim[1] * nchan_out * len(supocts)
+        self.mlp = MLP(inmlp, num_classes, hiddims=mlp_hidden,
+                       activate_out=nn.LogSoftmax(dim=1),
+                       batchnorm=[True]*len(mlp_hidden))
+
+    def forward(self, x):
+        out = torch.cat([
+            layer(x).view(len(x), -1) for layer in self.strflayers], -1)
+        out = self.mlp(out)
+        return out
