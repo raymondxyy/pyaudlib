791de82c6171f06742108a8872ff17040a97ba39
Raymond Xia <raymondxia@cmu.edu>
2019-09-04 23:07:25 -0400

    Add test cases for UnpackedSequence.

--- tests/nn/test_rnn.py
+++ tests/nn/test_rnn.py
@@ -1,7 +1,8 @@
 """Test suite for RNNs."""
 import torch
+from torch.nn.utils.rnn import pack_sequence
 
-from audlib.nn.rnn import ARMA, ExtendedLSTM
+from audlib.nn.rnn import ARMA, ExtendedLSTM, UnpackedSequence
 
 #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
@@ -26,6 +27,17 @@ def test_LSTM():
     return
 
 
+def test_UnpackedSequence():
+    """Test nn.rnn.UnpackedSequence."""
+    print(f"Testing UnpackedSequence with [{device}].")
+    seqs = [torch.rand(20 if i < 10 else 15, 256) for i in range(15)]
+    ps = pack_sequence(seqs)
+    for s1, s2 in zip(UnpackedSequence(ps), seqs):
+        assert torch.allclose(s1, s2)
+
+    return
+
 if __name__ == "__main__":
     test_ARMA()
     test_LSTM()
+    test_UnpackedSequence()
