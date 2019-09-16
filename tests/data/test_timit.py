060919b64cd097589e7b09ec516befb3a447fce2
Raymond Xia <raymondxia@cmu.edu>
2019-09-16 02:12:27 -0400

    Update TIMIT with cleaner interface. Add some frequently used transforms.

--- tests/data/test_timit.py
+++ tests/data/test_timit.py
@@ -1,17 +1,23 @@
 """Test suite for TIMIT."""
 import os
-from audlib.data.timit import TIMIT
+from audlib.data.timit import TIMIT, utt_no_shorter_than, randselwave, \
+    randselphon, isvowel
 
 
 def test_timit():
     SI = TIMIT('/home/xyy/data/timit',
-               filt=lambda p: 'SI' in os.path.basename(p).upper(),
-               readmode='utterance')
-    SX = TIMIT('/home/xyy/data/timit',
-               filt=lambda p: 'SX' in os.path.basename(p).upper(),
-               readmode='rand-phone')
+               filt=lambda p: 'SI' in os.path.basename(p).upper() and
+                              utt_no_shorter_than(p, 5),
+               transform=randselwave)
+    sample = SI[0]
+    print(sample)
 
-    print(SX, SI)
+    SI = TIMIT('/home/xyy/data/timit',
+               filt=lambda p: 'SI' in os.path.basename(p).upper() and
+                              utt_no_shorter_than(p, 5),
+               transform=lambda s: randselphon(s, isvowel))
+    sample = SI[0]
+    print(sample)
 
 
 if __name__ == "__main__":
