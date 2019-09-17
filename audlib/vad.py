dd308f4b198c64238cbfe5539a2d936ec82297d2
Raymond Xia <raymondxia@cmu.edu>
2019-09-17 07:28:45 -0400

    Add a simple energy-based voice activity detector.

--- audlib/vad.py
+++ audlib/vad.py
@@ -1,5 +1,6 @@
 """Methods for Voice (or Speech) Activity Detection."""
 import numpy as np
+from scipy.signal import lfilter
 
 from .sig.stproc import numframes, stana
 from .sig.temporal import zcrate, lpc_parcor, lpcerr
@@ -31,3 +32,47 @@ class ZCREnergy(object):
         out = out - out.mean(axis=0)
         out = out / out.std(axis=0)
         return out
+
+
+class SpectralEnergy(object):
+    """A spectral-energy-based voice activity detector.
+
+    This simple energy-based VAD operates on short-time power spectra with a
+    linear frequency scale (normal STFT).
+
+    """
+
+    def __init__(self, sr, nfft, fmin=300., fmax=4000.):
+        """Instantiate a VAD class.
+
+        Keyword Parameters
+        ------------------
+        fmin: float, 300
+            Minimum frequency from which energy is collected.
+        fmax: float, 4000
+            Maximum frequency from which energy is collected.
+
+        """
+        assert (fmin > 0) and (fmin < fmax) and (fmax < sr/2)
+        fintv = sr / nfft
+        self.bmin = int(fmin//fintv)
+        self.bmax = int(min(fmax//fintv, nfft//2))  # inclusive
+        self.nfft = nfft
+
+    def __call__(self, pspec, dbfloor=-30., smoothframes=0):
+        """Detect speech-active frames from a power spectra.
+
+        Keyword Parameters
+        ------------------
+        dbfloor: float, -30
+            Energy floor for a frame below maximum energy to be considered speech.
+        smoothframes: int, 0
+            Number of frames to apply a moving-average filter on power contour.
+
+        """
+        assert pspec.shape[1] == (self.nfft//2+1), "Incompatible dimension."
+        pspec = pspec[:, self.bmin:self.bmax+1].sum(axis=1)
+        if smoothframes > 0:
+            pspec = lfilter(1/smoothframes * np.ones(smoothframes), 1, pspec)
+        return pspec > (10**(dbfloor/10))*pspec.max()
+
