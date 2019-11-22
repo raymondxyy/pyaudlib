Fix bugs in ccep* calculation.

audlib/sig/cepstral.py
@@ -3,7 +3,7 @@ from functools import reduce
 import warnings
 
 import numpy as np
-from numpy.fft import rfft, irfft
+from numpy.fft import rfft, irfft, fft, ifft
 
 from .spectral import magphase, logmag
 
@@ -124,7 +124,7 @@ def ccep_zt(sig, n):
 
     """
     assert sig[0] != 0, "Leading zero!"
-    cep = np.empty(2*n-1)
+    cep = np.zeros(2*n-1)
     (rminc, rminr), (rmaxc, rmaxr), gain = roots(sig)
     if rmaxc.size > 0:
         for jj, ii in enumerate(range(-n+1, 0)):
@@ -168,10 +168,9 @@ def ccep_dft(sig, n, nfft=4096, floor=-80.):
     """
     warnings.warn("Unstable implementation. Use ccep_zt instead.")
     assert n <= nfft//2, "Consider larger nfft!"
-    spec = rfft(sig, n=nfft)
-    cep = irfft(clog(spec, floor=floor), n=nfft)[:2*n-1]
-    cep = np.roll(cep, n-1)
-    return cep
+    spec = rfft(sig, nfft)
+    cep = irfft(clog(spec, floor), nfft)
+    return np.concatenate((cep[-(n-1):], cep[:n]))
 
 
 def rcep_zt(sig, n):
@@ -197,8 +196,8 @@ def rcep_zt(sig, n):
 
     """
     ccep = ccep_zt(sig, n)
-    rcep = .5*(ccep+ccep[::-1])
-    return rcep[n-1:]  # only keep non-negative quefrency
+    rcep = .5*(ccep[n-1:]+ccep[n-1::-1])  # only keep non-negative quefrency
+    return rcep
 
 
 def rcep_dft(sig, n, nfft=4096, floor=-80.):
@@ -223,5 +222,4 @@ def rcep_dft(sig, n, nfft=4096, floor=-80.):
     cep: 1-D ndarray
 
     """
-    rcep = irfft(logmag(rfft(sig, nfft), floor), nfft)
-    return rcep[:n]
+    return irfft(logmag(rfft(sig, nfft), floor), nfft)[:n]

tests/sig/test_cepstral.py
@@ -5,7 +5,7 @@ from audlib.quickstart import welcome
 from audlib.sig.cepstral import rcep_zt, rcep_dft, ccep_zt, ccep_dft
 
 
-def test_ceps():
+def test_cceps():
     """Test complex cepstrum function using the impulse response of an echo.
 
     Taken from example 8.3 of RS, page 414.
@@ -20,25 +20,26 @@ def test_ceps():
 
     cepsize = 150
     # construct reference
-    cepref = np.zeros(cepsize)
-    for kk in range(1, cepsize // Np+1):
-        cepref[kk*Np] = (-1)**(kk+1) * (alpha**kk)/kk
+    cepref = np.zeros(2*cepsize-1)
+    deltalocs = np.zeros_like(cepref, dtype='bool')
+    deltalocs[cepsize-1+Np::Np] = True
+    cepref[deltalocs] = np.array( # first delta at x[Np]
+        [(-1)**(k+1) * (alpha**k)/k for k in range(
+            1, cepref[deltalocs].size+1)]
+    )
 
     # test complex cepstrum
-    ratcep = ccep_zt(x, cepsize)
-    dftcep = ccep_dft(x, cepsize)
-    assert np.allclose(cepref, ratcep[cepsize-1:])
-    assert np.allclose(cepref, dftcep[cepsize-1:])
+    ccep1 = ccep_zt(x, cepsize)
+    ccep2 = ccep_dft(x, cepsize)
+    assert np.allclose(cepref, ccep1)
+    assert np.allclose(cepref, ccep2, atol=1e-6)
 
     # test real cepstrum
-    cepref /= 2  # complex cepstrum is right-sided
+    cepref = .5*(cepref + cepref[::-1])[cepsize-1:]
     rcep1 = rcep_zt(x, cepsize)
     rcep2 = rcep_dft(x, cepsize)
-    rcep3 = ccep_zt(x, cepsize)
-    rcep3 = .5*(rcep3 + rcep3[::-1])[cepsize-1:]
     assert np.allclose(cepref, rcep1)
     assert np.allclose(cepref, rcep2)
-    assert np.allclose(cepref, rcep3)
 
 
 def test_rceps():
@@ -54,4 +55,4 @@ def test_rceps():
 
 if __name__ == "__main__":
     test_rceps()
-    test_ceps()
+    test_cceps()