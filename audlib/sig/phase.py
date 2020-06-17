78b6ba5bc039503a6781b481236c80213643b2a4
Raymond Xia <xyy@mailbox.org>
2020-06-16 23:10:07 -0400

    Fix OLA bug for griffin_lim.

--- audlib/sig/phase.py
+++ audlib/sig/phase.py
@@ -1,14 +1,13 @@
 """Some algorithms for PHASE estimation."""
 import numpy as np
-from numpy.fft import irfft
+from numpy.fft import irfft, rfft
 
 from .transform import stft
-from .stproc import ola
+from .stproc import ola, stana, hop2hsize
 from .spectral import magphasor
 
 
-def griffin_lim(magspec, wind, hop, nfft, niter=32, init=None, zphase=True,
-                reference=None):
+def griffin_lim(magspec, wind, hop, nfft, niter=32, init=None):
     """The Griffin-Lim iterative phase reconstruction method.
 
     This implementation follows Mark Lindsey's MATLAB implementation.
@@ -27,6 +26,10 @@ def griffin_lim(magspec, wind, hop, nfft, niter=32, init=None, zphase=True,
     init: numpy.ndarray or float
         Normalized initial phase value in (x\pi) radians. Default to zero phase.
 
+    Returns
+    -------
+    phasor, err
+
     """
     we = (wind**2).sum()
     assert we > 0, "Window must not be complete zeros!"
@@ -36,31 +39,38 @@ def griffin_lim(magspec, wind, hop, nfft, niter=32, init=None, zphase=True,
     if init is None:
         phasor = np.exp(1j*np.pi*(np.random.random_sample(magspec.shape)*2-1))
     elif type(init) in [float, int]:
-        phasor = np.full_like(magspec, np.exp(1j*np.pi*init))
+        phasor = np.full_like(magspec, np.exp(1j*np.pi*init), dtype='complex128')
     else:
         assert magspec.shape == init.shape, "Incompatible init phase dimension."
         phasor = np.exp(1j*np.pi*init)
 
     # Define inverse STFT with weighted signal
+    def stft_(sig):
+        frames = stana(sig, wind, hop)
+        return rfft(frames, nfft)
+
     def istft_(spec):
-        frames = irfft(spec, n=nfft)
-        if zphase:
-            fsize = len(wind)
-            woff = (fsize-(fsize % 2)) // 2
-            frames = np.concatenate((frames[:, (nfft-woff):],
-                                     frames[:, :(fsize-woff)]), axis=1)
-        else:
-            frames = frames[:, :len(wind)]
-        return ola(frames * wind / we, wind, hop)
+        """NOTE: stproc.ola does not work for GL because of alignment."""
+        frames = irfft(spec, n=len(wind)) * wind / we
+        nframe = len(frames)
+        fsize = len(wind)
+        hsize = hop2hsize(wind, hop)
+        ssize = hsize*(nframe-1)+fsize
+        ii = 0
+
+        sout = np.zeros(ssize)
+        for frame in frames:
+            frame = frame[:fsize]
+            sout[ii:ii+fsize] += frame
+            ii += hsize
+
+        return sout
 
     err = []
-    for ii in range(niter):
+    for _ in range(niter):
         sig = istft_(magspec * phasor)
-        _, phasor = magphasor(
-            stft(sig, wind, hop, nfft, synth=True, zphase=zphase)
-        )
+        magspec_, phasor = magphasor(stft_(sig))
+        err.append(((magspec-magspec_[:len(magspec)])**2).mean())
         phasor = phasor[:len(magspec)]
-        if reference is not None:
-            err.append(((sig[:len(reference)]-reference)**2).mean())
 
-    return phasor, err
+    return sig, err
--- tests/sig/test_phase.py
+++ tests/sig/test_phase.py
@@ -1,5 +1,7 @@
 """Test suite for audlib.sig.phase."""
+import numpy as np
 import soundfile as sf
+
 from audlib.sig.transform import stft, istft
 from audlib.sig.window import hamming
 from audlib.sig.util import nextpow2
@@ -13,17 +15,24 @@ WELCOME, SR = welcome()
 def test_griffin_lim():
     """Test STFT and iSTFT."""
     sig = WELCOME
-    wlen = .025
-    hop = .25
-    wind = hamming(int(wlen*SR), hop=hop, synth=True)
+    wlen = .032
+    hop = 128
+    wsize = int(wlen*SR)
+
+    # modified Hamming window
+    a = .54
+    b = -.46
+    phi = np.pi / wsize
+    wind = 2/(4*a**2+2*b**2)**.5*(a+b*np.cos(2*np.pi*np.arange(wsize)/wsize+phi))
+
+
+    wind = hamming(wsize, hop=hop)
     nfft = nextpow2(len(wind))
-    spec = stft(sig, wind, hop, nfft, synth=True)
+    spec = stft(sig, wind, hop, nfft)
     magspec, _ = magphasor(spec)
-    for zp in (True, False):
-        phasor, err = griffin_lim(magspec, wind, hop, nfft, zphase=zp,
-                                  reference=sig)
-        print(err)
-        sf.write(f"welcome-gl-zp_{zp}.wav", istft(magspec*phasor, wind, hop, nfft, zp), SR)
+
+    sig, err = griffin_lim(magspec, wind, hop, nfft, 100, init=0)
+    assert all(e1 >= e2 for (e1, e2) in zip(err[:-1], err[1:]))
 
 
 if __name__ == '__main__':
